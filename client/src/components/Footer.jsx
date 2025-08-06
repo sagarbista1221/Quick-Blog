8ed4166f521200b3ed4ff845cacfac15021e1624
sagarbista1221 <sagarbista1221@gmail.com>
2025-08-06 13:36:37 +0545

    Initial commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { assets, footer_data } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+        <div>
+          <img src={assets.logo} alt="logo" className="w-32 sm:w-44" />
+          <p className="max-w-[410px] mt-6">
+            This is the best blog website found in www about health finance news
+            and more
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+          {footer_data.map((section, index) => (
+            <div key={index}>
+              <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">
+                {section.title}
+              </h3>
+              <ul className="text-sm space-y-1">
+                {section.links.map((link, i) => (
+                  <li key={i}>
+                    <a href="#" className="hover:underline transtition">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+        Copyright 2025 © QuickBlog SAGAR - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
