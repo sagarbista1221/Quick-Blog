92c64147135f5b476ff70a87bf88a1064cbd53f7
sagarbista1221 <emigarsagar@gmail.com>
2025-08-21 08:30:30 +0545

    Delete server directory

--- server/routes/newsletterRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { subscribeUser } from "../controllers/newsletterController.js";
-
-const router = express.Router();
-
-router.post("/subscribe", subscribeUser);
-
-export default router;
--- server/routes/subscriberRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllSubscribers } from "../controllers/subscriberController.js";
-
-const router = express.Router();
-
-router.get("/", getAllSubscribers); // GET all subscribers
-
-export default router;
